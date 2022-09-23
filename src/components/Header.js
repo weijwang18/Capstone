dc73941a6bf17a4892d66c9086942ffa6feacf9d
Winnie Wang <weijwang18@gmail.com>
2022-09-23 14:08:03 -0700

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import React from "react";
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
-function Header(){
-  return (
-    <React.Fragment>
-      <h1>Hello!!</h1>
-    </React.Fragment>
-  );
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
 }
 
-export default Header;
+export default function BasicBreadcrumbs() {
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+        >
+          Snap Now
+        </Link>
+        <Link underline="hover" color="inherit" href="/">
+          History
+        </Link>
+      </Breadcrumbs>
+    </div>
+  );
+}
