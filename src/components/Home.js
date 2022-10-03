15464c72cbfa5aa06309ac87115f453f7d549ed1
Winnie Wang <weijwang18@gmail.com>
2022-10-03 10:07:27 -0700

    WIP: Building a grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import './Home.css';
 
 function Home(props){
     return (
       <React.Fragment>
-        <h1>This is Home</h1>
+        <div className="angry-grid">
+          <div id="item-0"></div>
+          <div id="item-1"></div>
+          <div id="item-2"></div>
+        </div>
       </React.Fragment>
     );
   }
